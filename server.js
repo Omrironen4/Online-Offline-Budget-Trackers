3e48b567cbc91c7788d47c2744a97c0fbea65e07
Omri <omri.ronen4@gmail.com>
2021-08-02 14:15:41 -0700

    deploying

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(express.static("public"));
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost/Workout-tracker', {
+  process.env.MONGODB_URI || 'mongodb://localhost/budgetTracker', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
